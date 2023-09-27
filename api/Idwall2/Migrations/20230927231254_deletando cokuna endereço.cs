d2889e28cb5dffdb0f93d145f247e0b07e6fa333
Wagner  Morais <waguinhoxd4@gmail.com>
2023-09-27 20:34:02 -0300

    Api Cadastro Suspeito

---
+++ api/Idwall2/Migrations/20230927231254_deletando cokuna endereço.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Idwall.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletandocokunaendereço : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ENDERECO",
+                table: "Suspeito");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ENDERECO",
+                table: "Suspeito",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
