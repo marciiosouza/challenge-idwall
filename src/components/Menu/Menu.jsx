3b126ffdb5c1fdc174748c554fdd2d370ba18d67
marciiosouza <marciiosouza@outlook.com>
2023-09-30 19:01:10 -0300

    Instalando biblioteca react-scroll e inserindo ancoragem na página home

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,3 +1,38 @@
+// import './Menu.css';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Button from 'react-bootstrap/Button';
+// import logoNav from '../../assets/images/logo-idwall.svg';
+// import { useNavigate } from 'react-router-dom';
+
+// export const Menu = () => {
+//   const navigate = useNavigate ()
+//   return (
+//     <Navbar expand="lg" className="nav-bg">
+//       <Container>
+//         <Navbar.Brand href="#home">
+//           <img id="logo-nav" src={logoNav} alt="Logo idwall" /></Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="mx-auto inner-text">
+//             <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+//             <Nav.Link href="#servicos_" className="nav-link">Serviços</Nav.Link>
+//             <Nav.Link href="#sobre-o-projeto" className="nav-link">Sobre o Projeto</Nav.Link>
+//             <Nav.Link href="#contato" className="nav-link">Contato</Nav.Link>
+//           </Nav>
+//           <ion-icon name="person-outline" className="icon-login"></ion-icon>
+//           <Button variant="" className="btn-login" onClick={() => navigate ("/login")} >Login</Button>
+//           <Button variant="" className="btn-cadastrar" onClick={() => navigate ("/cadastro")}>Cadastrar</Button>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   )
+// }
+
+// export default Menu
+
+// Menu.js
 import './Menu.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -5,29 +40,31 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import logoNav from '../../assets/images/logo-idwall.svg';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-scroll'; 
 
 export const Menu = () => {
-  const navigate = useNavigate ()
+  const navigate = useNavigate()
   return (
-    <Navbar expand="lg" className="nav-bg">
+    <Navbar expand="lg" className=" nav-bg bg-body-tertiary fixed-top">
       <Container>
-        <Navbar.Brand href="#home">
-          <img id="logo-nav" src={logoNav} alt="Logo idwall" /></Navbar.Brand>
+        <Navbar.Brand>
+          <img id="logo-nav" src={logoNav} alt="Logo idwall" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto inner-text">
-            <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
-            <Nav.Link href="#servicos" className="nav-link">Serviços</Nav.Link>
-            <Nav.Link href="#sobre-o-projeto" className="nav-link">Sobre o Projeto</Nav.Link>
-            <Nav.Link href="#contato" className="nav-link">Contato</Nav.Link>
+          <Nav className="mx-auto inner-text cursor-pointer">
+            <Link to="home" smooth={true} duration={500} className="nav-link">Home</Link>
+            <Link to="servicos" smooth={true} duration={500} className="nav-link">Serviços</Link>
+            <Link to="sobre-o-projeto" smooth={true} duration={500} className="nav-link">Sobre o Projeto</Link>
+            <Link to="contato" smooth={true} duration={500} className="nav-link">Contato</Link>
           </Nav>
-          {/* <ion-icon name="person-outline" className="icon-login"></ion-icon> */}
+          <ion-icon name="person-outline" className="icon-login"></ion-icon>
           <Button variant="" className="btn-login" onClick={() => navigate ("/login")} >Login</Button>
           <Button variant="" className="btn-cadastrar" onClick={() => navigate ("/cadastro")}>Cadastrar</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,55 +4,63 @@ import Cards from '../../components/Cards/Cards';
 import ImgNotbook from '../../assets/images/ilustra-sys.png';
 import ImgPhone from '../../assets/images/simetry-phone.svg';
 import { useNavigate } from 'react-router-dom';
+import { Element } from 'react-scroll'; 
 
 export default function Home() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
   return (
     <>
-      <section id="banner">
-        <div className="container">
-          <div className="row">
+      <Element name="home"> 
+        <section id="banner">
+          <div className="container">
+            <div className="row">
               <div className="col-md-12 col-sm-12 col-12 col-lg-6 mb-md-5 mb-5">
                 <h2 className="tittle-home">Plataforma de <br /> Gerenciamento de Consultas</h2>
                 <p className="descricao-home">O objetivo principal deste sistema é fornecer uma solução eficiente e intuitiva para o gerenciamento das pessoas procuradas pelo FBI e Interpol. Por meio da integração com as APIs do FBI e Interpol, poderemos obter informações atualizadas sobre os suspeitos e facilitar o trabalho das forças policiais na localização e captura desses indivíduos.</p>
                 <Button variant="" className="button-home" onClick={() => navigate("/login")}>Saiba mais sobre nossa solução!</Button>
               </div>
-            
-              <div className="col-md-12 col-sm-12 col-12 col-lg-6 text-center">
-              <img id="notbook-banner" src={ImgNotbook} alt="Notebook exibindo uma imagem de um sistema de cadastro." />
-            </div>
-          </div>
-        </div>
-      </section>
-      <main className="mb-5">
-        <section>
-          <h2 className="destaque-home">Serviços</h2>
-          <Cards />
-        </section>
-        <section>
-          <h2 className="destaque-home">Sobre o Projeto</h2>
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 col-lg-6 mb-5 text-center">
-                <img id="notbook-banner" src={ImgPhone} alt="Notebook exibindo uma imagem de um sistema de cadastro." />
-              </div>
-              <div className="col-md-12 col-lg-6">
-                <p className="paragraph-home">Este projeto tem como objetivo desenvolver um sistema para gerenciar as pessoas mais procuradas pelo FBI e Interpol, utilizando uma API REST para obter os dados das respectivas organizações.</p>
-                <p className="paragraph-home">O objetivo principal deste sistema é fornecer uma solução eficiente e intuitiva para o gerenciamento das pessoas procuradas pelo FBI e Interpol. Por meio da integração com as APIs do FBI e Interpol, poderemos obter informações atualizadas sobre os suspeitos e facilitar o trabalho das forças policiais na localização e captura desses indivíduos.</p>
 
-                <a
-                  className="button-repository mb-5"
-                  href="https://github.com/marciiosouza/Challenge_Idwall"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <ion-icon name="logo-github" id="icons"></ion-icon> Repositório no Github
-                </a>
+              <div className="col-md-12 col-sm-12 col-12 col-lg-6 text-center">
+                <img id="notbook-banner" src={ImgNotbook} alt="Notebook exibindo uma imagem de um sistema de cadastro." />
               </div>
             </div>
           </div>
         </section>
+      </Element>
+      <main className="mb-5">
+        <Element name="servicos"> 
+          <section>
+            <h2 className="destaque-home">Serviços</h2>
+            <Cards />
+          </section>
+        </Element>
+        <Element name="sobre-o-projeto"> 
+          <section>
+            <h2 className="destaque-home">Sobre o Projeto</h2>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12 col-lg-6 mb-5 text-center">
+                  <img id="notbook-banner" src={ImgPhone} alt="Notebook exibindo uma imagem de um sistema de cadastro." />
+                </div>
+                <div className="col-md-12 col-lg-6">
+                  <p className="paragraph-home">Este projeto tem como objetivo desenvolver um sistema para gerenciar as pessoas mais procuradas pelo FBI e Interpol, utilizando uma API REST para obter os dados das respectivas organizações.</p>
+                  <p className="paragraph-home">O objetivo principal deste sistema é fornecer uma solução eficiente e intuitiva para o gerenciamento das pessoas procuradas pelo FBI e Interpol. Por meio da integração com as APIs do FBI e Interpol, poderemos obter informações atualizadas sobre os suspeitos e facilitar o trabalho das forças policiais na localização e captura desses indivíduos.</p>
+
+                  <a
+                    className="button-repository mb-5"
+                    href="https://github.com/marciiosouza/Challenge_Idwall"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <ion-icon name="logo-github" id="icons"></ion-icon> Repositório no Github
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
+        </Element>
       </main>
     </>
-  )
+  );
 }
